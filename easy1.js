30fed1c1588c36c756ad76d03ff9e43406b5d38c
Hemant Govekar <hemantgovekar@gmail.com>
2023-02-18 19:46:04 +0530

    refactored easy 1 problem

--- easy1.js
+++ easy1.js
@@ -9,20 +9,13 @@ function twoSum(nums, target) {
         }
         mp.set(nums[i], i);
     }
+
+    throw new Error("Match not found");
+
+
 }
 
 let nums = [2, 17, 11, 7],
     target = 19;
 console.log(twoSum(nums, target));
 
-// const map = new Map(); // create a new Map to store seen numbers
-
-// for (let i = 0; i < nums.length; i++) {
-//   const complement = target - nums[i]; // calculate the complement
-
-//   if (map.has(complement)) {
-//     return [map.get(complement), i]; // return indices of the two numbers
-//   }
-
-//   map.set(nums[i], i); // store the current number and its index in the map
-// }
